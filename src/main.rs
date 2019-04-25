2d7c1442b755325688be5c664cbffa5b64676ec6
soruh <mail@soruh.de>
2019-04-25 16:45:47 +0200

    insert HOME into repo_path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-// #[macro_use]
-// extern crate clap;
-// use clap::App;
+#[macro_use]
+extern crate clap;
+use clap::App;
 
 extern crate config;
 extern crate serde;
@@ -13,12 +13,12 @@ mod settings;
 use settings::Settings;
 
 fn main() {
-    let settings = Settings::new();
+    let mut settings = Settings::new();
 
-    // Print out our settings
-    println!("{:?}", settings);
+    settings.insert_home_dir();
 
-    /*
+    // println!("{:?}", settings);
+
     // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml)
@@ -30,28 +30,49 @@ fn main() {
     let (name, args) = matches.subcommand();
 
     if let Some(args) = args {
-        println!("args: {:#?}", args);
+        // println!("args: {:#?}", args);
 
         match name {
-            "templates" => templates(args),
-            "init" => init(args),
-            "convert" => convert(args),
+            "templates" => templates(&settings, args),
+            "init" => init(&settings, args),
+            "convert" => convert(&settings, args),
             _ => panic!("recieved unexpected subcommand"),
         }
     } else {
-        panic!("missing arguments");
+        panic!("no arguments found");
     }
-    */
 }
 
-/*
-fn templates(args: &clap::ArgMatches) {
-    //
+fn init(settings: &Settings, args: &clap::ArgMatches) {
+    println!(
+        "\x1b[32mcommand\x1b[0m: {}\n\x1b[33msettings\x1b[0m: {:#?}\n\x1b[34margs\x1b[0m: {:#?}",
+        "init", settings, args.args
+    );
+
+    // TODO: allow ssh dirs
+
+    // * 0. calculate repo dir
+    // ? 1. if dir exists:
+    //      ! abort
+    // ? 2. if template ist set:
+    //      * Either: copy template dir
+    //      * Or    : run template script
+    // ? 3. if .git doesn't exist:
+    //      * git init [Folder]
+    // ? 4. if file changes:
+    //      * add & commit all changed files (-> template files)
+    // ? 5. if remote configured:
+    //      * setup remote
 }
-fn init(args: &clap::ArgMatches) {
-    //
+fn templates(settings: &Settings, args: &clap::ArgMatches) {
+    println!(
+        "\x1b[32mcommand\x1b[0m: {}\n\x1b[33msettings\x1b[0m: {:#?}\n\x1b[34margs\x1b[0m: {:#?}",
+        "template", settings, args.args
+    );
 }
-fn convert(args: &clap::ArgMatches) {
-    //
+fn convert(settings: &Settings, args: &clap::ArgMatches) {
+    println!(
+        "\x1b[32mcommand\x1b[0m: {}\n\x1b[33msettings\x1b[0m: {:#?}\n\x1b[34margs\x1b[0m: {:#?}",
+        "convert", settings, args.args
+    );
 }
-*/
--- src/settings.rs
+++ src/settings.rs
@@ -7,7 +7,7 @@ use std::process::Command;
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
-    debug: bool,
+    repo_path: String,
 }
 
 fn load_config_path(s: &mut Config, config_path: &str) {
@@ -29,7 +29,7 @@ fn copy_default_config(config_path: &str) {
     let exe_path = env::current_exe().expect("failed to get executable path");
     let default_config_path = exe_path
         .parent()
-        .expect("WTF")
+        .unwrap()
         .to_path_buf()
         .join("../../default.config")
         .canonicalize()
@@ -43,11 +43,39 @@ fn copy_default_config(config_path: &str) {
 }
 
 impl Settings {
+    pub fn insert_home_dir(&mut self) {
+        let home = match env::var("HOME") {
+            Ok(home) => Some(home),
+            Err(_) => None,
+        };
+
+
+        loop{ //TODO: expand to all paths
+            // since repo_path is the only one for now, this is fine
+
+
+            if self.repo_path.find("~") == None {
+                continue;
+            }
+
+            // TODO: check if path is local (no method in "url")
+
+            match home {
+                None => panic!("a config path references '~', but $HOME is not set"),
+                Some(home) => {
+                    self.repo_path = self.repo_path.replace("~", home.as_ref());
+                }
+            }
+
+            break;
+        }
+    }
+
     pub fn new<'a>() -> Self {
         let mut config_paths = vec!["/etc/repoman"];
 
         let mut path1 = String::new();
-        let mut path2 = String::new(); //TODO: can this be in the if scope somehow?
+        let mut path2 = String::new(); //TODO: can this be inside of the if scope somehow?
 
         let home = env::var("HOME");
         if let Ok(home) = home {
@@ -76,7 +104,7 @@ impl Settings {
             load_config_path(&mut s, config_path.as_ref());
         } else {
             eprintln!("no config found.");
-            eprintln!("please choose one of the following locations, to create one:");
+            eprintln!("please choose one of the following locations to create one:");
 
             for i in 0..config_paths.len() {
                 eprintln!("{}: {}", i, config_paths[i]);
@@ -104,7 +132,7 @@ impl Settings {
                 config_path = Some(config_paths[path_num].to_string());
                 break;
             }
-            let config_path = config_path.expect("WTF");
+            let config_path = config_path.unwrap();
             copy_default_config(config_path.as_ref());
             load_config_path(&mut s, &config_path.as_ref());
         }
